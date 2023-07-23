173bf9c90e0835ed723418601ef71f89bb56c8ff
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-23 10:25:21 -0600

    Set up sessions with cookies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const routes = require('./controllers');
 const exphbs = require('express-handlebars'); 
 const path = require('path');
 
@@ -9,6 +10,17 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 // Set the view engine to use Handlebars
 app.engine('hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
@@ -16,7 +28,13 @@ app.set('view engine', 'hbs');
 // Set the views directory to point to the correct location
 app.set('views', path.join(__dirname, 'views'));
 
+// Middleware
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use(routes);
 
 // Sync the database and start the server
