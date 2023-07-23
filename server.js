65ab0665580f26a30bd5416cc046f09116015b35
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-23 10:04:29 -0600

    view engine and handlebars included for

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-
+const exphbs = require('express-handlebars'); 
+const path = require('path');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -8,6 +9,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set the view engine to use Handlebars
+app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+// Set the views directory to point to the correct location
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(routes);
