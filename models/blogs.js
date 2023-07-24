d3217f45a191f02a867a87bde16dea4622a52605
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-23 18:08:53 -0600

    users and blogs models loaded with defining columns

--- models/blogs.js
+++ models/blogs.js
@@ -1,39 +1,41 @@
+// models/blogs.js
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Blogs extends Model {}
 
 Blogs.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-            },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-            },
-        content: {
-            type: DataTypes.STRING,
-            allowNull: false
-            },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-                }
-            }
-        },
-        {
-            sequelize,
-            timestamps: false,
-            freezeTableName: true,
-            underscored: true,
-            modelName: 'blogs',
-          }
-        );
-        
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: '', 
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogs',
+  }
+);
+
 module.exports = Blogs;
