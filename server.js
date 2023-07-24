d2cd8590aa1058f1a826f6483d52a9780edde2ff
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-23 18:06:40 -0600

    sessions setup with cookies

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars'); 
-const path = require('path');
-
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers'); 
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,31 +14,34 @@ const PORT = process.env.PORT || 3001;
 // Set up sessions with cookies
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
-// Set the view engine to use Handlebars
-app.engine('hbs', exphbs({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-
-// Set the views directory to point to the correct location
-app.set('views', path.join(__dirname, 'views'));
-
-// Middleware
 app.use(session(sess));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use(routes);
 
-// Sync the database and start the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT}`
+    )
+  );
 });
