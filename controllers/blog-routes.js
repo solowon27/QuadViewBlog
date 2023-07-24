1d2a6d221815b247fa087e9db1c3ed48ce3d6817
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-23 18:08:03 -0600

    blog and user routes handle all http requests

---
+++ controllers/blog-routes.js
@@ -0,0 +1,83 @@
+// blog-routes.js
+const express = require('express');
+const router = express.Router();
+const { Blogs } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const blogPosts = await Blogs.findAll();
+  const blogs = blogPosts.map((blog) => blog.get({ plain: true }));
+  res.render('homepage', { blogs, 
+    logged_in: req.session.logged_in,
+    year: new Date().getFullYear() });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', async (req, res) => {
+  try {
+    const blogPosts = await Blogs.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+    const blogs = blogPosts.map((blog) => blog.get({ plain: true }));
+    res.render('dashboard', { blogs, 
+      logged_in: req.session.logged_in,
+      year: new Date().getFullYear() });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blogs/:id', async (req, res) => {
+  try {
+    const blogPost = await Blogs.findByPk(req.params.id);
+    const blog = blogPost.get({ plain: true });
+    res.render('blogpost', { blog, 
+      logged_in: req.session.logged_in,
+      year: new Date().getFullYear() });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/blogs', async (req, res) => {
+  try {
+    const blogPost = await Blogs.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(blogPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/blogs/:id', async (req, res) => {
+  try {
+    const blogPost = await Blogs.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(blogPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/blogs/:id', async (req, res) => {
+  try {
+    const blogPost = await Blogs.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(blogPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
