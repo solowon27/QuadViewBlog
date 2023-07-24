a5e2875b13fc2fb57e924f920d746c9999b788ce
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-24 11:53:52 -0600

    models association implemented

--- models/index.js
+++ models/index.js
@@ -1,13 +1,32 @@
 const User = require('./users');
 const Blogs = require('./blogs');
+const Comment = require('./comment');
 
+// Associations between User and BlogPost
 User.hasMany(Blogs, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
 });
 
 Blogs.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
 });
 
-module.exports = { User, Blogs };
+// Associations between User and Comment
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+// Associations between BlogPost and Comment
+Blogs.hasMany(Comment, {
+  foreignKey: 'blogs_id',
+});
+
+Comment.belongsTo(Blogs, {
+  foreignKey: 'blogs_id',
+});
+
+module.exports = { User, Blogs, Comment };
