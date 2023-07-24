a5843b728f560e9990cd3512e2ec48f48dd9bc0e
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-23 21:37:13 -0600

    old codes destroyed by git reset --hard HEAD~1

--- controllers/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const blogRoutes = require('./blog-routes');
-
-router.use('/blogs', blogRoutes);
-
-module.exports = router;
--- seeds/user-seeds.js
+++
@@ -1,8 +0,0 @@
-const { User } = require('../models'); // Make sure the path to the User model is correct
-const userData = require('./user-seeds.json');
-
-const seedUsers = async () => {
-  await User.bulkCreate(userData);
-};
-
-module.exports = seedUsers;
