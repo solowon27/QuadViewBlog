53b5128e658dbefd4674ab2f59889bf28e073337
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-25 10:45:31 -0600

    blogpost.js handle only new blog posts

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -1,56 +1,29 @@
-// blogpost.js
-document.addEventListener('DOMContentLoaded', async () => {
-  
-    const commentForm = document.getElementById('commentForm');
-  const loggedInUser = document.getElementById('loggedInUser').value;
+async function submitNewBlogPost(event) {
+  event.preventDefault();
 
-  if (loggedInUser) {
-    commentForm.style.display = 'block'; // Show the comment form if the user is logged in
-    commentForm.addEventListener('submit', submitComment);
-  }
+  // Get the form data
+  const formData = new FormData(newBlogForm);
+  const title = formData.get('title');
+  const content = formData.get('content');
 
-  async function submitComment(event) {
-    async function submitComment(event) {
-      event.preventDefault();
-      const commentText = document.getElementById('commentText').value.trim();
-  
-      if (commentText) {
-        const blogId = window.location.pathname.split('/').pop();
-        const response = await fetch(`/blogs/${blogId}/comments`, {
-          method: 'POST',
-          body: JSON.stringify({ text: commentText }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-  
-        if (response.ok) {
-          // Clear the comment input and refresh the comments section
-          document.getElementById('commentText').value = '';
-          await loadComments();
-        } else {
-          alert('Failed to submit comment. Please try again.');
-        }
-      }
-    }
-  
-    async function loadComments() {
-      const blogId = window.location.pathname.split('/').pop();
-      const response = await fetch(`/blogs/${blogId}`);
-      const blogData = await response.json();
-  
-      const commentsElement = document.getElementById('comments');
-      commentsElement.innerHTML = '';
-  
-      // Display comments
-      if (blogData.comments && blogData.comments.length > 0) {
-        blogData.comments.forEach(comment => {
-          const commentElement = document.createElement('li');
-          commentElement.textContent = `${comment.text} - by ${comment.user.username}`;
-          commentsElement.appendChild(commentElement);
-        });
-      }
-    }
-  
-    await loadComments();
+  // Get the logged-in user's ID from the session 
+  const creator = req.session.user_id;
+
+  // Send a POST request to create the new blog post with the creator value
+  const response = await fetch('/blogs', {
+    method: 'POST',
+    body: JSON.stringify({ title, content, creator }), // Include the creator value
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    // Clear the form after successful submission
+    newBlogForm.reset();
+    // Load the updated blog posts
+    await loadBlogPosts();
+  } else {
+    alert('Failed to create a new blog post. Please try again.');
   }
-});
-  
+}
