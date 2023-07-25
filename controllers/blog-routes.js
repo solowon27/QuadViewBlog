5da6d797d99433ec890b095ed10ec24e61ce3272
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-25 10:37:20 -0600

    Get the logged-in user's ID from the session and render

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -1,15 +1,16 @@
-// blog-routes.js
 const express = require('express');
 const router = express.Router();
-const { Blogs } = require('../models');
+const { Blogs, User } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
     const blogPosts = await Blogs.findAll();
-  const blogs = blogPosts.map((blog) => blog.get({ plain: true }));
-  res.render('homepage', { blogs, 
-    logged_in: req.session.logged_in,
-    year: new Date().getFullYear() });
+    const blogs = blogPosts.map((blog) => blog.get({ plain: true }));
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in,
+      year: new Date().getFullYear(),
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -17,13 +18,25 @@ router.get('/', async (req, res) => {
 
 router.get('/dashboard', async (req, res) => {
   try {
-    const blogPosts = await Blogs.findAll();
-    const blogs = blogPosts.map((blog) => blog.get({ plain: true }));
-    res.render('dashboard', { blogs, 
-      logged_in: req.session.logged_in,
-      year: new Date().getFullYear() });
+    // Get the logged-in user's ID from the session
+    const userId = req.session.user_id;
+
+    // If the user is not logged in, redirect to the login page
+    if (!userId) {
+      return res.redirect('/login');
+    }
+
+    // Find all blog posts belonging to the logged-in user
+    const blogs = await Blogs.findAll({
+      where: { user_id: userId }, // Use correct column name user_id
+      include: [{ model: User, attributes: ['username'] }],
+    });
+
+    // Render the dashboard view with the user's blog posts
+    res.render('dashboard', { loggedIn: true, username: req.session.username, blogs });
   } catch (err) {
-    res.status(500).json(err);
+    console.log(err);
+    res.status(500).json({ message: 'Failed to load dashboard.' });
   }
 });
 
@@ -31,9 +44,11 @@ router.get('/blogs/:id', async (req, res) => {
   try {
     const blogPost = await Blogs.findByPk(req.params.id);
     const blog = blogPost.get({ plain: true });
-    res.render('blogpost', { blog, 
+    res.render('blogpost', {
+      blog,
       logged_in: req.session.logged_in,
-      year: new Date().getFullYear() });
+      year: new Date().getFullYear(),
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -42,8 +57,9 @@ router.get('/blogs/:id', async (req, res) => {
 router.post('/blogs', async (req, res) => {
   try {
     const blogPost = await Blogs.create({
-      ...req.body,
-      user_id: req.session.user_id,
+      title: req.body.title,
+      content: req.body.content,
+      creator: req.session.username, // Set the creator value
     });
     res.status(200).json(blogPost);
   } catch (err) {
