42cebb8920aec7c4499b6b1b8730d6a95fc495be
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-07-26 20:21:49 -0600

    fetching comments done using blogs id

--- controllers/comment-routes.js
+++ controllers/comment-routes.js
@@ -1,20 +1,43 @@
-
 const express = require('express');
 const router = express.Router();
-const { Comment } = require('../models');
+const { Comment, User, Blogs } = require('../models');
 
-router.post('/comments', async (req, res) => {
+router.get('/blogs/:id/comments', async (req, res) => {
   try {
-    const newComment = await Comment.create({
-      text: req.body.commentText,
-      blogId: req.body.blogId, 
-      userId: req.session.user_id,
+    const postId = req.params.id;
+    // Query the database to get comments for the specified blog post
+    const comments = await Comment.findAll({
+      where: { blogId: postId },
+      include: [{ model: User, attributes: ['username'] }],
     });
 
-    res.status(200).json(newComment);
+    // Respond with the comments in JSON format
+    res.status(200).json({ comments });
   } catch (err) {
-    console.log(err); // Log the error for debugging purposes
-    res.status(500).json({ message: 'Failed to submit the comment.' });
+    console.error(err);
+    res.status(500).json({ message: 'Failed to fetch comments.' });
+  }
+});
+
+router.post('/blogs/:id/comments', async (req, res) => {
+  try {
+    // Extract the blog post ID from the request parameters
+    const blogId = req.params.id;
+
+    // Extract the comment text from the request body
+    const { text } = req.body;
+
+    // Create the new comment
+    const newComment = await Comment.create({
+      text, 
+      blogId: blogId, // Set the blogId to the ID of the blog post
+      user_id: req.session.user_id, // Set the user_id to the ID of the logged-in user
+    });
+
+    res.status(201).json(newComment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to create a new comment. Please try again.' });
   }
 });
 
